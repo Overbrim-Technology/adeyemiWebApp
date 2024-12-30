dd480bbcc5e6d720d87976aac8a824489581c7cd
Timi-G <56408619+Timi-G@users.noreply.github.com>
2024-12-30 21:41:29 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./static/templates/**/*.{html,js}",
+            "./blog/templates/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      haus: ['At Hauss', 'sans-serif'],
+    },
+  },
+  plugins: [require('daisyui'),],
+}
