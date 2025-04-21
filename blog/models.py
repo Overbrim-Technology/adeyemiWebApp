107545b46680323616347d62562e32df329492e2
Timi-G <56408619+Timi-G@users.noreply.github.com>
2025-04-21 05:38:43 +0000

    Added blog article page

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -11,8 +12,21 @@ class Article(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200)
     category = models.ForeignKey(Category,on_delete=models.CASCADE, related_query_name='category')
+    # slug = models.SlugField(unique=True, blank=True, null=True)
+    description = models.TextField()
     body = models.TextField()
     image = models.URLField()
 
     def __str__(self):
         return self.title
+
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         timestamp = self.created.strftime('%Y%m%d%H%M%S') if self.created else ''
+    #         self.slug = slugify(f"{self.title}-{timestamp}")
+    #     super().save(*args, **kwargs)
+
+    # def get_absolute_url(self):
+    #     from django.urls import reverse
+    #     timestamp = self.created.strftime('%Y%m%d%H%M%S')
+    #     return reverse("article_detail", args=[self.category, timestamp, self.slug])
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.core.paginator import Paginator
 
 from .models import Article, Category
-
 
 # Create your views here.
 def blog(request):
@@ -13,5 +14,14 @@ def blog(request):
     page_number= request.GET.get('page')
     page_obj= paginated.get_page(page_number)
 
-    # return render(request,'blog.html')
     return render(request,'blog.html',{'page_obj':page_obj, 'blog_posts':blog_posts, 'categories':categories})
+
+def article(request, category, date_time, slug):
+    # article page url will be format adeyemiodubeko.com/{category-datetime}
+    try:
+        created = datetime.strptime(date_time, '%Y%m%d%H%M%S')
+    except ValueError:
+        return render(request, '404.html', status=404)
+
+    # article = get_object_or_404(Article, category=category, created=created, slug=slug)
+    return render(request, 'blog_article.html', {'article': article})
